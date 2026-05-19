30a85f0f7ad02ed9a3ab2cc1d0b11c30f4d18513
Hossein199700 <alirahmani999877@gmail.com>
2026-05-19 11:25:47 +0330

    Create data.dart

---
+++ lib/data.dart
@@ -0,0 +1,15 @@
+import 'models.dart';
+
+List<Student> students = [
+  Student("Ali", 20),
+  Student("Sara", 50),
+  Student("Reza", 30),
+];
+
+List<String> assignments = [
+  "Math Homework",
+  "Science Project",
+  "English Essay"
+];
+
+List<String> feedbacks = [];
